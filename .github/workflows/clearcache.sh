ecc149bddb907d8110698d89f358516c60aefac6
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2023-09-10 00:25:26 +0800

    aaaa

--- .github/workflows/clearcache.sh
+++
@@ -1,6 +0,0 @@
-# Auth
-echo "$CRED" > /secrets.json
-gcloud auth activate-service-account --key-file=/secrets.json
-rm /secrets.json
-
-gcloud compute url-maps invalidate-cdn-cache stats-web-balancer-2 --path "/*"
