ce2fb4d949b984629ec99bc967772c95ac2810fb
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-13 03:35:46 +0800

    rebuild

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ Number.prototype.floored = function (fractionDigits = 0) {
 	let pow = Math.pow(10, fractionDigits);
 	return Math.floor(this * pow) / pow;
 };
+//
 
 import { App } from "./renderer/App.js";
 
