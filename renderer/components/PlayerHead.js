3d27f9d9d53c241d5bf39f5b20a05499696aacef
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-01-31 18:26:49 +0800

    better head

--- renderer/components/PlayerHead.js
+++ renderer/components/PlayerHead.js
@@ -1,10 +1,27 @@
 import { css, html, staticCss, thisClass } from "../../soopyframework/helpers.js";
 
-let headCss = staticCss.named("head").css`${thisClass} {
+let fakeImgCss = staticCss.named("fake-img").css`${thisClass} {
 	aspect-ratio: 1;
 	width: auto;
-	image-rendering: crisp-edges;
+	height: auto;
+	display: inline-block;
+	padding-right: 2px;
+}`
+
+let headCss = fakeImgCss.named("head").css`{
+	*:has(> ${thisClass}) { /*parent*/
+		position: relative;
+	}
+
+	${thisClass} {
+		image-rendering: pixelated;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
 }`;
+
 
 /**
  * @param uuid
@@ -14,9 +31,13 @@ let headCss = staticCss.named("head").css`${thisClass} {
  */
 export function PlayerHead(uuid, options={}) {
 	return html`
+		<div ${fakeImgCss} ${css`
+			${options.width ? `width: ${options.width};` : ""}
+			${options.height ? `height: ${options.height};` : ""}
+		`}></div>
 		<img src="https://api.soopy.dev/skin/${uuid}/head.png" width="8" height="8" ${headCss} ${css`
-			${options.width ? `width: ${options.width};`:""}
-			${options.height ? `height: ${options.height};`:""}
+			${options.width ? `width: ${options.width};` : ""}
+			${options.height ? `height: ${options.height};` : ""}
 		`}>
 	`;
 }
