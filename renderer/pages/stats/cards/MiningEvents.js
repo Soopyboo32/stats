aa5bd5d9ddaef4ab58d70cc2ff6ce348b18d1282
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-02-01 19:08:43 +0800

    total lobbys

--- renderer/pages/stats/cards/MiningEvents.js
+++ renderer/pages/stats/cards/MiningEvents.js
@@ -18,8 +18,8 @@ export function MiningEvents() {
 	return Card(html`<span ${ref}>Mining Events</span>`, eventData.observe(() => {
 		if (!eventData.get().data) return html`Loading...`;
 
-		let totalChLobbys = eventData.get().data.data.running_events.CRYSTAL_HOLLOWS.reduce((acc, cur) => acc + cur.lobby_count, 0)
-		let totalMinesLobbys = eventData.get().data.data.running_events.DWARVEN_MINES.reduce((acc, cur) => acc + cur.lobby_count, 0)
+		let totalChLobbys = eventData.get().data.data.total_lobbys.CRYSTAL_HOLLOWS || 0;
+		let totalMinesLobbys = eventData.get().data.data.total_lobbys.DWARVEN_MINES || 0;
 
 		return html`
 			<b>Crystal Hollows:</b>
