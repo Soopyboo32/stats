5b39aa56f816e50b9287cef85b10fd97a2ec48ce
Soopyboo32 <49228220+Soopyboo32@users.noreply.github.com>
2024-07-08 14:06:29 +0800

    delay loading some reasources cus service worker will load them anyway

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { html } from "./soopyframework/helpers.js";
+
 Number.prototype.floored = function (fractionDigits = 0) {
 	let pow = Math.pow(10, fractionDigits);
 	return Math.floor(this * pow) / pow;
@@ -7,3 +9,12 @@ Number.prototype.floored = function (fractionDigits = 0) {
 import { App } from "./renderer/App.js";
 
 document.getElementById("app").outerHTML = App();
+
+let elm = document.createElement("div");
+elm.innerHTML = html`
+	<link rel="stylesheet" href="./global.css">
+	<link rel="stylesheet"
+		  href="./itemtextures/inventory.css" />
+	<link rel="icon" href="https://avatars.githubusercontent.com/u/49228220?v=4">
+`
+document.body.appendChild(elm);
